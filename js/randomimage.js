4e23cbeeb60d10431ee48f499f31c78f3adcbef8
Chr P <christian.pintson@tktk.ee>
2022-11-06 16:39:17 +0200

    esimene

---
+++ js/randomimage.js
@@ -0,0 +1,26 @@
+const myButton = document.querySelector('button');
+const image = document.querySelector('img');
+const images = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg"];
+let tempIndex = 0;
+
+
+myButton.addEventListener('click', changeImage);
+
+function changeImage() {
+
+    
+    document.getElementById('info-text').innerText = ''
+    let randomIndex = Math.floor(Math.random() * images.length)
+    if (randomIndex == tempIndex) {
+        
+        document.getElementById('info-text').innerText = 'Sama pilt mis varem!'
+        
+    } else {
+        
+        image.src = "/images/" + images[randomIndex]
+        tempIndex = randomIndex
+
+        }
+    
+    
+}
